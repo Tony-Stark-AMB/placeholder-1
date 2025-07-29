f0ec6273aac612141f8145c66d00a88e1ae1a344
Tony-Stark-AMB <tony.stark.amb@gmail.com>
2025-07-29 19:59:11 +0300

    change import ./Countdown.css => ./CountDown.css

--- src/components/CountDown/CountDown.jsx
+++ src/components/CountDown/CountDown.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Countdown.css';
+import './CountDown.css';
 
 const Countdown = ({ targetDate }) => {
   const calculateTimeLeft = () => {
